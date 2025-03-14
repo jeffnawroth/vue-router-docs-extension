27065a12ed5c63f8faec2147f234416a229abd40
Jeff Nawroth <idol.analysiert_0n@icloud.com>
2025-03-14 16:57:02 +0100

    feat: implement sectioned documentation search with filtering

--- src/search-documentation.tsx
+++ src/search-documentation.tsx
@@ -1,31 +1,66 @@
-import { ActionPanel, Action, Icon, List } from "@raycast/api";
+import { ActionPanel, Action, List } from "@raycast/api";
+import sections from "./sections";
+import { SectionFilter, SectionFilterString } from "./types";
+import { useState } from "react";
 
-const ITEMS = Array.from(Array(3).keys()).map((key) => {
-  return {
-    id: key,
-    icon: Icon.Bird,
-    title: "Title " + key,
-    subtitle: "Subtitle",
-    accessory: "Accessory",
-  };
-});
+type State = {
+  filter: SectionFilter;
+  searchText: string;
+};
 
 export default function Command() {
+  const [state, setState] = useState<State>({
+    filter: SectionFilter.ALL,
+    searchText: "",
+  });
+
+  const filteredSections = (() => {
+    if (state.filter === SectionFilter.ALL) {
+      return sections ?? [];
+    }
+
+    return sections?.filter((section) => section.title === state.filter) ?? [];
+  })();
+
   return (
-    <List>
-      {ITEMS.map((item) => (
-        <List.Item
-          key={item.id}
-          icon={item.icon}
-          title={item.title}
-          subtitle={item.subtitle}
-          accessories={[{ icon: Icon.Text, text: item.accessory }]}
-          actions={
-            <ActionPanel>
-              <Action.CopyToClipboard content={item.title} />
-            </ActionPanel>
-          }
-        />
+    <List
+      searchText={state.searchText}
+      searchBarAccessory={
+        <List.Dropdown
+          tooltip="Select Section"
+          value={state.filter}
+          onChange={(newValue) => setState((previous) => ({ ...previous, filter: newValue as SectionFilter }))}
+        >
+          {Object.keys(SectionFilter).map((key) => (
+            <List.Dropdown.Item
+              key={key}
+              title={SectionFilter[key as SectionFilterString]}
+              value={SectionFilter[key as SectionFilterString]}
+            />
+          ))}
+        </List.Dropdown>
+      }
+      filtering
+      onSearchTextChange={(newValue) => {
+        setState((previous) => ({ ...previous, searchText: newValue }));
+      }}
+    >
+      {filteredSections.map((section) => (
+        <List.Section key={section.title} title={section.title}>
+          {section.items.map((item) => (
+            <List.Item
+              key={item.title}
+              title={item.title}
+              icon="v-logo.png"
+              actions={
+                <ActionPanel>
+                  <Action.OpenInBrowser url={item.url} />
+                  <Action.CopyToClipboard content={item.url} title="Copy URL" />
+                </ActionPanel>
+              }
+            />
+          ))}
+        </List.Section>
       ))}
     </List>
   );
